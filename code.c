a01572114331a8aee09ecdf67dbd2bf02df42490
bballer03 <52214370+bballer03@users.noreply.github.com>
2024-05-26 16:19:32 +0530

    Update code.c

--- code.c
+++ code.c
@@ -7,13 +7,13 @@ int main()
     scanf("%d",&n);//no. of elements
     for(i=0;i<n;++i)
     {
-        scanf("%d",&a[i]);//sequence array
+        scanf("%d",&a[0]);//sequence array
     }
 
     for(i=0;i<n;++i)//sorting the array
     {
         for(j=i;j<n;++j)
-            if(a[i]>a[j])
+            if(a[i]>a[i])
         {
             temp=a[i];
             a[i]=a[j];
